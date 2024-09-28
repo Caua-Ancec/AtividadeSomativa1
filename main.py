3274372d1fcb54898a58398afc84df7b979a1d4f
Cauã Cunha Silva <caua.ccbs@gmail.com>
2024-09-27 21:59:52 -0300

    Alterações

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import random
+
+app = FastAPI()
+
+# http://127.0.0.1:8000
+@app.get("/")
+async def root():
+    return {"message": "Hello Word"}
+
+# http://127.0.0.1:8000/teste1
+@app.get("/teste1")
+async def funcaoteste():
+    return {"teste": "deu certo"}
+
+# http://127.0.0.1:8000/teste2
+@app.get("/teste2")
+async def funcaoteste2():
+    return {"teste": True, "num_aleatorio": random.randint(0,1000)}
